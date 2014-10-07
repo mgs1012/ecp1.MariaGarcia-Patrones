0bae45e3fac83c3b3c183cd947902ef5bdb9eafd
bl0823 <bl0823@192.168.7.20>
2014-10-07 10:33:31 +0200

    Añadir operacion “Multiplication”

---
+++ src/main/java/ecp1/patrones/Multiplication.java
@@ -0,0 +1,15 @@
+package ecp1.patrones;
+
+public class Multiplication extends MathOperations{
+
+	public Multiplication(int operator1, int operator2) {
+		super(operator1, operator2);
+	}
+
+	@Override
+	public int calcularOperacion() {
+		return 0;
+	}
+	
+
+}
