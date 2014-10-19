b51c10c59b343e954f9562c17203b439ed31959e
Maria <Maria@Maria-PC>
2014-10-20 00:10:27 +0200

    Terminada aplicación vehiculos alquiler

--- src/main/java/ecp1/patrones/AplicacionAlquilerVehiculos/VehiculoMain.java
+++ src/main/java/ecp1/patrones/AplicacionAlquilerVehiculos/VehiculoMain.java
@@ -4,8 +4,11 @@ import upm.jbb.IO;
 
 public class VehiculoMain {
 	
+	//App vehiculos IO --> ejecutar
+	
 	public static void main(String[] args) {
 		VehiculoController controller = new VehiculoController();
 		IO.out.addController(controller);
 	}
+	
 }
